896a060be52d19d28451649925a0a2a5a5b8b409
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-14 21:39:44 +0200

    Resolucion rutas home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        component: HomeComponent,
+    },
     {
         path: "register-form",
         component: RegisterComponent
