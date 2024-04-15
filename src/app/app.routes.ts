16a061ca074ca44c29e469a1b2f05a05c29240c6
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-15 21:55:05 +0200

    Componente page not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,13 @@ export const routes: Routes = [
     {
         path: "register-form",
         component: RegisterComponent
-    }
+    },
+    { 
+        path: 'not-found', 
+        component: NotFoundComponent 
+    },
+    { 
+        path: '**', 
+        redirectTo: 'not-found'  // Redirecciona todas las rutas no encontradas a 'not-found'
+    }, 
 ];
