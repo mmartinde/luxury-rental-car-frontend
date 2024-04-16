8da1d51b2f846c2b87039844b5f4f2f71843565a
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-16 21:19:06 +0200

    Bug menu-collpase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes)
+  ]
 };
