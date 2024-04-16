9bdffcd67d04bb8db3f6a30d919628153a50b50b
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-16 22:40:40 +0200

    User login_falta conectar backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     },
     {
         path: "register-form",
--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../../../interfaces/forms-data/user-login';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from '../../../services/user.service.spec';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  showLoginForm: boolean = true;
+  hidePassword: boolean = true;
 
+  ngOnInit() {}
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private userService: UserService,
+    private cookies: CookieService,
+    private router: Router
+  ) {}
+
+  loginForm: FormGroup = this.formbuilder.group({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, Validators.required),
+  });
+
+  onSubmit() {
+    if (this.showLoginForm && this.loginForm.valid) {
+      const data: UserLogin = {
+        email: this.loginForm.get('email')?.value,
+        password: this.loginForm.get('password')?.value,
+      };
+
+      this.userService.login(data).subscribe({
+        next: (res: any) => {
+          this.userService.setTokenRole(res.token, res.role),
+            this.router.navigate(['/']); //navega a la ruta privada que tenda destinada ese rol
+          console.log(res);
+        },
+        error: (err) => console.log(err),
+      });
+    }
+  }
 }
