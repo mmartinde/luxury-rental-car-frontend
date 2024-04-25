8105a08b70e7e18f2321c9551577eb98f9f81bc9
javierpelaez97 <javierpelaezgimenez@gmail.com>
2024-04-25 23:19:10 +0200

    Filtros creados a falta de unirlos entre si

--- src/app/components/find-car/find-car.component.ts
+++ src/app/components/find-car/find-car.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { Cars } from '../../interfaces/cars';
 import { FormGroup, FormBuilder, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CardComponent } from '../card/card.component';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-find-car',
@@ -18,8 +19,12 @@ export class FindCarComponent implements OnInit {
 
   carsFiltered: Cars []=[]
 
+  
+
   searcherCars: FormGroup = this.FormBuilder.group({
-    car : new FormControl(null, [Validators.required])
+    car : new FormControl(null, [Validators.required]),
+    transmission: new FormControl(null),
+    power: new FormControl(null)
   })
 
   constructor(
@@ -42,9 +47,43 @@ export class FindCarComponent implements OnInit {
     console.log('Coche filtrado', carFilter)
     this.carsFiltered = this.cars.filter((i)=>i.make.toLocaleLowerCase().includes(carFilter))
     console.log('Este es el coche filtrado',this.carsFiltered);
+
+  }
+   searchCarTransmission(){
+    const carFilter  = this.searcherCars.get('transmission')?.value
+    console.log('Coche filtrado por transmisión automatica', carFilter);
+      if(carFilter === 'Automatic' || 'Manual'){
+        this.carsFiltered = this.cars.filter((i)=>i.transmission.includes(carFilter))
+        console.log('Este es el coche filtrado por transmission', this.carsFiltered);
+      }else(carFilter === 'select');{
+        this.carsFiltered
+        console.log('No hay transmisión seleccionada');
+        
+      }
+    
+  } 
+
+  searchCarPower(){
+    const carFilter = this.searcherCars.get('power')?.value
+    console.log('Coche filtrado por potencia', carFilter);
+      if(carFilter === 'descendant'){
+        this.carsFiltered= this.cars.sort((a,b)=>b.hp - a.hp);
+      console.log('Este es el orden desdendente', this.carsFiltered);
+      }else if(carFilter === 'ascendant'){
+        this.carsFiltered = this.cars.sort((a,b)=>a.hp - b.hp);
+        console.log('Este es el orden ascendente', this.carsFiltered);
+      }else(carFilter === 'select');{
+        this.carsFiltered
+        console.log('No hay potencia seleccionada');
+        
+      }
     
     
   }
+
+
+  
+
 
   
 
