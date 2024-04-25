a8f02418290c212ce29c298d7894c4ebc06004e0
Francisco Arteaga <93606979+arteaga137@users.noreply.github.com>
2024-04-25 15:32:12 +0200

    programa navegacion en boton "ver coche" en ruta Cars"

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Route, Router, RouterOutlet } from '@angular/router';
 import { FindCarComponent } from '../find-car/find-car.component';
 import { Cars } from '../../interfaces/cars';
 
@@ -8,10 +8,14 @@ import { Cars } from '../../interfaces/cars';
   standalone: true,
   imports: [RouterOutlet, FindCarComponent],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  @Input() car?: Cars;
 
-  @Input() car? :  Cars;
+  constructor(private route: Router) {}
 
+  navigateToCar(carId: string): void {
+    this.route.navigate([`cars/${carId}`]);
+  }
 }
