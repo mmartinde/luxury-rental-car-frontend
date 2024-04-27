ab2f36eb56c78f9858b76a042a3a8040d8e9b813
javierpelaez97 <javierpelaezgimenez@gmail.com>
2024-04-27 08:51:22 +0200

    Inicio carrousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CarsLayoutComponent } from '../../components/cars-layout/cars-layout.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarsLayoutComponent, ],
+  imports: [CarsLayoutComponent, CarouselComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
