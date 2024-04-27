03bb27344c5aa7b6b1bd3acea5a9a97d037b8227
Francisco Arteaga <93606979+arteaga137@users.noreply.github.com>
2024-04-27 11:58:46 +0200

    corrige logica logout, implementa condicional para mostrar solicitar reserva de acuerdo a autenticacion

--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarDetailComponent } from '../../components/car-detail/car-detail.component';
 import { RentRequestComponent } from '../../components/rent-request/rent-request.component';
-
 
 @Component({
   selector: 'app-car',
@@ -10,6 +9,13 @@ import { RentRequestComponent } from '../../components/rent-request/rent-request
   templateUrl: './car.component.html',
   styleUrl: './car.component.scss',
 })
-export class CarComponent {
- 
+export class CarComponent implements OnInit {
+  storedUserId: string | null = '';
+
+  ngOnInit(): void {
+    const sessionUserId = localStorage.getItem('userId');
+    if (sessionUserId !== null) {
+      this.storedUserId = sessionUserId;
+    }
+  }
 }
