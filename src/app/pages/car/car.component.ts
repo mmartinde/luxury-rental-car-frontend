3ea19a85d1e0ff925723b8e86ddfcf2f2f33224c
Francisco Arteaga <93606979+arteaga137@users.noreply.github.com>
2024-04-27 11:38:35 +0200

    corrige estructura de paginas y componentes del flujo de rentar coche

--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,69 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
-import { CarsService } from '../../services/cars.service';
-import { Cars } from '../../interfaces/cars';
-import { switchMap } from 'rxjs';
+import { Component } from '@angular/core';
+import { CarDetailComponent } from '../../components/car-detail/car-detail.component';
+import { RentRequestComponent } from '../../components/rent-request/rent-request.component';
+
 
 @Component({
   selector: 'app-car',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CarDetailComponent, RentRequestComponent],
   templateUrl: './car.component.html',
   styleUrl: './car.component.scss',
 })
 export class CarComponent {
-  car?: Cars;
-  carId: string = '';
-  isLoading = true;
  
-  // constructor(private carService: CarsService, private route: ActivatedRoute) {
-  //   this.route.params.pipe(
-  //     switchMap((params: Params) => {
-  //       this.carId = params['id'];
-  //       return this.carService.getCarById(this.carId);
-  //     })
-  //   ).subscribe({
-  //     next: (car) => {
-  //       this.car = car as Cars;
-  //       this.isLoading = false;
-  //     },
-  //     error: (err) => console.log('Error fetching car:', err)
-  //   });
-  // }
-  
-  constructor(private carService: CarsService, private route: ActivatedRoute, private router: Router) {
-    this.route.params.subscribe({
-      next: (res: any) => {
-        this.carId = res.id;
-        console.log(this.carId)
-        this.carService.getCarById(this.carId).subscribe({
-          next: (res) => (this.car = res as Cars, this.isLoading = false),
-          error: (err) => console.log(err),
-        });
-      },
-    })
-      }
-
-  // // ngOnInit(): void {
-  // //   // this.getCar
-  // // }
-
-  // // getCar(): void {
-  // //   if (id) {
-  // //     this.route.paramMap.subscribe((params) => {
-  // //       const id = params.get('id');
-  // //       if (id) {
-  // //         this.carService.getCarById(id).subscribe({
-  // //           next: (res) => (this.car = res as Cars ),
-  //                   this.isLoading = false
-  // //           error: (err) => console.log('No se ha encontrado el vehículo'),
-  // //         });
-  // //       }
-  // //     });
-  // //   }
-  // // }
-  
-  goToReservation(carId: any): void {
-    this.router.navigate([`/contact/${carId}`])
-  }
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,72 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { RentService } from '../../services/rent.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent implements OnInit {
-  rentForm: FormGroup;
-  carId: string = '';
-  userId: string = '';
-
-  constructor(
-    private route: ActivatedRoute,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private rentService: RentService
-  ) {
-    this.rentForm = this.formBuilder.group({
-      dateIn: ['', Validators.required],
-      dateOut: ['', Validators.required],
-      car: [this.carId, Validators.required],
-      user: [this.userId, Validators.required],
-      price: ['1200', Validators.required], // Donde tenemos los precios?
-      status: ['2', Validators.required],
-    });
-  }
-
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    const storedUserId = localStorage.getItem('userId');
-    // console.log(storedUserId)
-    if (storedUserId !== null) {
-      this.userId = storedUserId;
-      // console.log(id)
-    }
-    if (id !== null) {
-      this.carId = id;
-    } else {
-      console.error('No existe ID de coche en los parametros de ruta');
-      this.router.navigate(['/error']); // Crearemos una pagina de error, o lo redirige a un 404?
-    }
-    this.rentForm = this.formBuilder.group({
-      dateIn: ['', Validators.required],
-      dateOut: ['', Validators.required],
-      car: [id, Validators.required],
-      user: [storedUserId, Validators.required],
-      price: ['1200', Validators.required],
-      status: ['2', Validators.required],
-    });
-  }
-  submitRentRequest() {
-    if (this.rentForm.valid) {
-      console.log(this.rentForm.value);
-      this.rentService.createRent(this.rentForm.value).subscribe({
-        next: (res: any) => this.router.navigate(['/requestSent']),
-        error: (err) => console.error('No se pudo solicitar la renta: ', err),
-      });
-    }
-  }
+export class ContactComponent {
+  
 }
