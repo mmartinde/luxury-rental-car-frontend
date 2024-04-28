cc8d0454c2db9b2951edb47ce8c69c641a9ace92
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-28 17:41:22 +0200

    form alquiler coches usuario

---
+++ src/app/components/adminUsers/form-rental-user/form-rental-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-rental-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-rental-user.component.html',
+  styleUrl: './form-rental-user.component.scss'
+})
+export class FormRentalUserComponent {
+
+}
