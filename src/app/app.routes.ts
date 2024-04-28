cd4ba4592ee1cc0864bee40ad499015723248f6b
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-04-28 22:03:59 +0200

    form listar alquiler coches usuario-falla la logica de back, busca por id del rent y no por id de user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { EditCarComponent } from './components/admin-car/edit-car/edit-car.compo
 import { AddCarComponent } from './components/admin-car/add-car/add-car.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { RequestSentComponent } from './pages/request-sent/request-sent.component';
+import { UserRentComponent } from './pages/user-rent/user-rent.component';
 
 export const routes: Routes = [
     {
@@ -30,7 +31,13 @@ export const routes: Routes = [
     },
     { 
         path: 'user', 
-        component: UserComponent
+        component: UserComponent,
+        canActivate:[userProtectGuard]
+    },
+    {
+        path:"user/rent",
+        component:UserRentComponent,
+        canActivate:[userProtectGuard]
     },
     {
         path:'cars',
--- src/app/components/adminUsers/form-rental-user/form-rental-user.component.ts
+++ src/app/components/adminUsers/form-rental-user/form-rental-user.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { RentService } from '../../../services/rent.service';
+import { UserService } from './../../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Rent } from '../../../interfaces/rent';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-rental-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './form-rental-user.component.html',
   styleUrl: './form-rental-user.component.scss'
 })
-export class FormRentalUserComponent {
+export class FormRentalUserComponent implements OnInit{
+  userId: string | undefined
+  rentForm: FormGroup
+  rent: Rent[]=[]
 
+constructor(
+  private rentService:RentService,
+  private formBuilder: FormBuilder,
+  private router: Router
+){
+  this.rentForm=this.formBuilder.group({
+    car:['', Validators.required],
+    user:['', Validators.required],
+    dateIn:['', Validators.required],
+    dateOut:[''],
+    price:[''],
+    status:['', Validators.required],
+  })
+}
+
+
+ngOnInit(): void {
+  const userId:any = localStorage.getItem('userId')
+  this.rentService.getRentbyId(userId).subscribe({
+    next:(res:any)=>{
+      this.rent=res as Rent[],
+      this.rentForm.patchValue(this.rent)
+    },
+    error:(err:string)=>console.log('Error al obtner alquileres', err)
+  })
+}
+
+onSubmit():void{
+  const userId:any=localStorage.getItem("userId")
+  if(this.rentForm.valid){
+    console.log(this.rentForm.value)
+    this.rentService.editRent(userId, this.rentForm.value).subscribe({
+      next:(res:any)=> this.router.navigate(['user/rent']),
+      error:(err:string)=>console.error('Error modificar renta', err)
+    });
+  }    
+}
 }
